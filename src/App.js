82fa47c52598852fc01f9993ae43b45c6d14e5f0
Ben (Macbook) <benbromley.104@gmail.com>
2021-08-04 23:20:49 +0100

    added all components into App() component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Landing from './components/Landing.jsx';
 
 function App() {
   return (
     <div>
-      <div className="App p-6 m-6 bg-white flex items-center rounded-lg max-w-xs mx-auto shadow-xl">
-        <p className="p-1 text-blue-500 text-xl font-medium">Hello</p>
-        <p className="p-1 text-green-300 text-sm">All</p>
-        </div>
+      <Header />
+      <Landing />
+      <Footer />
     </div>
   );
 }
