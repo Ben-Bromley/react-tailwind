12d06ea16c4fb834ea9b1bdf246372ef432db866
Ben (Macbook) <benbromley.104@gmail.com>
2021-08-04 23:21:03 +0100

    written JSX for footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <div>
+    	<div className="mb-6 max-w-sm mx-auto text-center">
+      	    <p>© Copyright 2021 <a className="text-blue-500 hover:text-blue-700 hover:underline" href="https://www.clubhouse.com/@phonebiddy">Connect with Shaquel Simpson-Kothari on Clubhouse.</a>All rights reserved.</p>
+      	    <p>Developed By <a className="text-gray-500 hover:text-blue-700 hover:underline" href="https://webhoot.co.uk/">Webhoot.co.uk.</a></p>
+    	</div>
+    </div>
+  );
+}
+
+export default Footer;
