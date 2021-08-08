b1c0896801adeba0f10e75a9c42eef2636422340
Ben (Macbook) <benbromley.104@gmail.com>
2021-08-08 13:13:14 +0100

    updated to use react routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
-// import '../App.css';
-
+import { Link } from 'react-router-dom';
 function Header() {
   return (
     <div>
       <nav className="bg-blue-600 text-white flex p-4 justify-between items-center">
       	<div><p id="title" className="font-bold"><span className="text-3xl">BACKLINKED.</span><span className="text-yellow-400 text-2xl">CLUB</span></p></div>
       	<div>
-      		<button className="bg-green-500 px-4 py-1 rounded-md hover:bg-green-600">Login </button>
+          <Link to="/login">
+            <button className="bg-green-500 px-4 py-1 rounded-md hover:bg-green-600">Login </button>
+          </Link>
       	</div>
       </nav>
     </div>
