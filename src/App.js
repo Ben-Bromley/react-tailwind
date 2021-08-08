71027a54bee2a58b2184c934edc5ef8e73e1d414
Ben (Macbook) <benbromley.104@gmail.com>
2021-08-08 13:12:53 +0100

    adapted app.js to use react routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import Landing from './components/Landing.jsx';
+import Login from './components/Login.jsx';
+import ErrorPage from './components/ErrorPage.jsx'
+
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Landing />
-      <Footer />
+      {<Header />}
+      <Switch>
+      	<Route path='/login' component={Login} />
+      	<Route exact path='/' component={Landing} />
+      	<Route component={ErrorPage} />
+      </Switch>
+      {<Footer />}
     </div>
   );
 }
